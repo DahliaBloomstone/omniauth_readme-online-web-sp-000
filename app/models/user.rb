51147b35e4bd3f4945a993b353d14db6c665fa44
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-08 02:03:56 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 # The User model should have four attributes, all strings: name, email, image, and uid (the user's ID on Facebook).
 class User
-  
+
 
 
 #In order to ensure Rails is using https, do the following to start the server instead of our normal rails s flow:
