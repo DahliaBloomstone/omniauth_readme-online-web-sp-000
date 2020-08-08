ec91c4553fce9f6f952b09f905bb6790b7bf6faa
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-08 02:06:53 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,3 +6,4 @@ class User
 #In order to ensure Rails is using https, do the following to start the server instead of our normal rails s flow:
 
 # thin start --ssl
+#Typically, if a matching User exists in your database, the client will be logged in to your application. If no match is found, a new User will be created using the data received from the provider.
